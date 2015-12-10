b8bb62d9919673f8cd87c08a2faf7f8b224db102
LuisEnVilla <villak@hotmail.es>
2015-12-10 13:53:52 -0600

    add checkout fields

--- Floryou-change.php
+++ Floryou-change.php
@@ -18,5 +18,47 @@ function custom_override_checkout_fields( $fields ) {
   return $fields;
 }
 
+/*Agregar campos al Checkout
+	Para poder usar el datepicker tenemos que agregar esto en el header
 
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		$( "input[name='date_shipping']" ).datepicker({
+			changeMonth: true,
+			changeYear: true,
+			dateFormat: 'yy-mm-dd' 
+			});
+		});
+	</script>
+*/
+add_filter( 'woocommerce_checkout_fields' , 'add_checkout_fields' );
+function add_checkout_fields( $fields ) {
+
+	$fields['shipping']['date_shipping'] = array(
+	'type'		=> 'text',
+	'label'		=> __('Fecha', 'woocommerce'),
+	'placeholder'   => _x('Fecha de envió...', 'placeholder', 'woocommerce'),
+	'required'	=> true,
+	'class'		=> array('form-row-wide'),
+	'clear'		=> true
+	);
+
+	$fields['shipping']['shipping_schedule'] = array(
+	'type'		=> 'select',
+	'label'		=> __('Horario', 'woocommerce'),
+	'required'	=> true,
+	'class'		=> array('form-row-wide'),
+	'clear'		=> true,
+	'options'	=> array(
+		'no_definido'	=> 'Selecciona el horario de envio...',
+		'mañana' => 'Por la mañana',
+		'tarde' => 'Por la Tarde',
+		'noche' => 'Por la noche')
+	);
+
+     return $fields;
+}
 ?>
