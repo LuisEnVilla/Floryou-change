1b3eb553b53db0546b2786d35216a948b65225d3
LuisEnVilla <villak@hotmail.es>
2015-12-27 17:48:09 -0600

    add to card text comprar

--- Floryou-change.php
+++ Floryou-change.php
@@ -4,10 +4,10 @@
 	*/
 
 	/*Cambiar el texto del boton para agregar al carrito...*/
-	add_filter( 'add_to_cart_text', 'texto_personalizado' );
+	add_filter( 'woocommerce_product_add_to_cart_text', 'texto_personalizado' );
 	add_filter( 'woocommerce_product_single_add_to_cart_text', 'texto_personalizado' );
 	function texto_personalizado() {
-		return __( 'Mi Texto ', 'woocommerce' );
+		return __( 'Comprar', 'woocommerce' );
 	}
 
 	/*Filtro para quitar fields del Checkout en Woocommerce WP*/
