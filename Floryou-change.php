cf1e5d7d60f77689b2804585d208456a4ba5be4a
LuisEnVilla <villak@hotmail.es>
2015-12-27 17:42:19 -0600

    woocommerce_product_single_add_to_cart_text

--- Floryou-change.php
+++ Floryou-change.php
@@ -5,7 +5,7 @@
 
 	/*Cambiar el texto del boton para agregar al carrito...*/
 	add_filter( 'add_to_cart_text', 'texto_personalizado' );
-	add_filter( 'woocommerce_product_add_to_cart_text', 'texto_personalizado' );
+	add_filter( 'woocommerce_product_single_add_to_cart_text', 'texto_personalizado' );
 	function texto_personalizado() {
 		return __( 'Mi Texto ', 'woocommerce' );
 	}
